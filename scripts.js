0b6e0906b25e43a1cc6e31f55c72e93d1d04d540
David Bronne <davidbronne@gmail.com>
2021-06-03 19:19:19 +0200

    progress bar set up

--- scripts.js
+++ scripts.js
@@ -2,14 +2,15 @@
 //  grab DOM elements
 const player = document.querySelector('.player')
 const viewer = player.querySelector('.viewer')
-/* console.log('viewer :>> ', {viewer}); */
+console.log('viewer :>> ', {viewer});
 const playPauseButton = player.querySelector('.toggle')
 /* console.log('playPauseButton :>> ', {playPauseButton}); */
 const skipButtons = player.querySelectorAll('[data-skip]')
 /* console.log('skipButtons :>> ', {skipButtons}); */
 const rangers = player.querySelectorAll('.player__slider');
-console.log('rangers :>> ', rangers);
-
+/* console.log('rangers :>> ', rangers); */
+const progressBar = player.querySelector('.progress__filled');
+console.log('progressBar :>> ', progressBar);
 
 // define functions
 function togglePlay () {
@@ -35,13 +36,19 @@ function handleSkip () {
 
 function handleChange () {
     viewer[this.name] = this.value
-    
+}
+
+function handleProgress () {
+    const progressValue = (viewer.currentTime / viewer.duration) * 100;
+    console.log('progressValue :>> ', progressValue);
+    progressBar.style.flexBasis = `${progressValue}%`
 }
 
 // fire function thanks to event listners
 viewer.addEventListener('click', togglePlay)
 viewer.addEventListener('play', toggleIcon)
 viewer.addEventListener('pause', toggleIcon)
+viewer.addEventListener('timeupdate', handleProgress)
 
 playPauseButton.addEventListener('click', togglePlay)
 
@@ -52,3 +59,5 @@ skipButtons.forEach( skipButton => {
 rangers.forEach( ranger => {
     ranger.addEventListener('change', handleChange)
 })
+
+
